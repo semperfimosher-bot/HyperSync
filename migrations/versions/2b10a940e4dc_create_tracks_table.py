c8c11f495a171342368c13f40fb485d053227a4c
Shane <semperfi.mosher@gmail.com>
2026-09-21 21:42:01 -0400

    test: make timestamp defaults dialect-aware

--- migrations/versions/2b10a940e4dc_create_tracks_table.py
+++ migrations/versions/2b10a940e4dc_create_tracks_table.py
@@ -36,13 +36,13 @@ def upgrade() -> None:
         sa.Column(
             "created_at",
             sa.DateTime(timezone=True),
-            server_default=sa.text("now()"),
+            server_default=sa.func.now(),
             nullable=False,
         ),
         sa.Column(
             "updated_at",
             sa.DateTime(timezone=True),
-            server_default=sa.text("now()"),
+            server_default=sa.func.now(),
             nullable=False,
         ),
         sa.PrimaryKeyConstraint("id", name=op.f("pk_tracks")),
